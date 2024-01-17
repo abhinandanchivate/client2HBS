7d8a6cff73b9d06e820caa2a9e0dfa6b384fd7de
abhinandanchivate <abhinandanchivate2000@gmail.com>
2024-01-17 12:30:35 +0530

    updated one

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import api from "./api";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [navbarTemplate, setNavbarTemplate] = useState("");
+  useEffect(() => {
+    api
+      .get("/users/navbar")
+      .then((response) => {
+        setNavbarTemplate(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return <div dangerouslySetInnerHTML={{ __html: navbarTemplate }}></div>;
 }
 
 export default App;
